b74a0d6694d6f39618f49d29e5e38ce2b32a8e45
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-02-10 00:53:02 +0530

    added tf file

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "public_tg_arn" {
-  value = aws_lb_target_group.public.arn
-}
+#output "public_tg_arn" {
+#  value = aws_lb_target_group.public.arn
+#}
 #the above output should referenced in terraform mutable infra module where u hv terrafile
