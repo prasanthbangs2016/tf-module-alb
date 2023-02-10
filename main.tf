7834637c2c2081ebd3be61aef9f756b7d683e07d
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-02-11 01:20:04 +0530

    added tf file

--- main.tf
+++ main.tf
@@ -84,6 +84,18 @@ resource "aws_lb_target_group" "public" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  deregistration_delay = 0
+
+  health_check {
+    enabled = true
+    healthy_threshold = 2
+    interval = 5
+    timeout = 4
+    port = 80
+    unhealthy_threshold = 2
+
+  }
+}
 }
 
 #6 if anyone hitting http,redirect to https(if anyone hitting 80 port redirect to https)
